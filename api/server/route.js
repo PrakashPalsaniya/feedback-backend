d0e739f23f755d83e1c9aa3f5d8aba458537a81b
Prakash Palsaniya <150008749+PrakashPalsaniya@users.noreply.github.com>
2025-07-29 22:39:23 +0530

    Update route.js

--- api/server/route.js
+++ api/server/route.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: '*',  
+  origin: 'https://feedback-frontend-updated.vercel.app',  
   credentials: true     
 }));
 
